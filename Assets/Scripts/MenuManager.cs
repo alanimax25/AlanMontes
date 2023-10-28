6717eb90585c56e788eb017a10fc9d8792d65907
alanimax25 <alan_radio@hotmail.com>
2023-10-28 13:11:23 -0600

    Lean Tween

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,6 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] private GameObject title;
+
+    private void Start()
+    {
+        //LeanTween.scale(title, new Vector3(1, 1, 1), 1).setEase(LeanTweenType.easeInExpo).setDelay(1);
+        LeanTween.scale(title, new Vector3(1, 1, 1), 0.5f).setLoopType(LeanTweenType.pingPong).setDelay(1);
+    }
+
+    private void Update()
+    {
+        //LeanTween.scale(title, new Vector3(1, 1, 1), 0.5f).setLoopType(LeanTweenType.pingPong).setDelay(1);
+    }
     public void Play(int sceneNumber)
     {
         SceneManager.LoadScene(sceneNumber);
